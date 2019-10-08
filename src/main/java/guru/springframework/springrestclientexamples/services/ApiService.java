ac35ae9eeecc475a11c164bbbd10c11484c1ed91
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2019-10-08 14:01:54 -0500

    Added RESTful API consumption tests for swagger toy API

--- src/main/java/guru/springframework/springrestclientexamples/services/ApiService.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiService.java
@@ -2,9 +2,11 @@ package guru.springframework.springrestclientexamples.services;
 
 import guru.springframework.api.domain.User;
 
+
 import java.util.List;
 
 public interface ApiService {
 
     List<User> getUsers(Integer limit);
+
 }
